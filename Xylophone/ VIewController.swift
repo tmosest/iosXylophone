f82963975c5a8ff861c7eb0a74754888d5527309
tmosest <tmosest@gmail.com>
2018-12-30 06:16:19 -0500

    first commit

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -9,21 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController{
-    
-
+  
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
-
     @IBAction func notePressed(_ sender: UIButton) {
-        
-        
-        
+      
     }
-    
-  
-
 }
 
